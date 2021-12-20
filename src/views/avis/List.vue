5dbb22c719a50863033e91ac2495bd289385f9b0
robertj1029 <robertj9216@gmail.com>
2021-12-19 20:30:44 -0500

    adsense ad code for approval

--- src/views/avis/List.vue
+++ src/views/avis/List.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+
+ <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3655622089482340"
+     crossorigin="anonymous"></script>
+<!-- vwratings home -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3655622089482340"
+     data-ad-slot="3406436587"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script> 
+  
     <EntitiesList entity="avi" method="avis"/>
   </div>
 </template>
