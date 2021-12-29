2adac9c8d31b82dd32339ee7b05b4d368d2d6d0a
robertj1029 <robertj9216@gmail.com>
2021-12-28 21:12:09 -0500

    added link to party gallery

--- src/components/entities/EntitiesGallery.vue
+++ src/components/entities/EntitiesGallery.vue
@@ -4,6 +4,8 @@
     <b-button
       class="mb-4 btn-back"
       @click="$router.go(-1)">Back</b-button>
+	<b-button
+      class="mb-4 ml-5 btn-back" :to="{name: 'ratings.parties.gallery'}">Party Pics</b-button>
 
     <b-pagination
       v-model="currentPage"
