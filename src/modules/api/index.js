b73b29832d19b76c130b0b964fcf0316d9516dc4
robertj1029 <robertj9216@gmail.com>
2022-08-04 23:15:17 -0400

    fix duplicate key issue

--- src/modules/api/index.js
+++ src/modules/api/index.js
@@ -8,7 +8,7 @@ import apiParties from './methods/parties';
 import apiNominations from './methods/nominations';
 import apiVotings from './methods/votings';
 import apiNominationsPeeps from './methods/nominations_peeps';
-import apiVotingsPeeps from './methods/votings';
+import apiVotingsPeeps from './methods/votings_peeps';
 export default {
   removeJWT() {
     Cookie.remove('access_token');
@@ -110,7 +110,7 @@ export default {
       nominations: apiNominations(this.apiInstance),
       votings: apiVotings(this.apiInstance),
       nominations_peeps: apiNominationsPeeps(this.apiInstance),
-      votings: apiVotingsPeeps(this.apiInstance),	  
+      votings_peeps: apiVotingsPeeps(this.apiInstance),	  
     };
 
     Vue.prototype.$api = repositories;
