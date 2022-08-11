3a1a1ab82001a8e69953b72065d7132b59744384
robertj1029 <robertj9216@gmail.com>
2022-08-10 22:11:35 -0400

    unhide buttons for live

--- src/views/buttons/PeepsCreeps.vue
+++ src/views/buttons/PeepsCreeps.vue
@@ -12,12 +12,12 @@
  <div v-if="adBlock">		
 		<b-row class="side">
         <div class="sidey text-center">
-          <b-button class="good">Peeps</b-button>
+          <b-button class="good" :to="{name: 'ratings.peeptimer'}">Peeps</b-button>
           <br />
           Peeps = Decent players
         </div>
         <div class="sidey text-center">
-          <b-button class="bad">Creeps</b-button>
+          <b-button class="bad" :to="{name: 'ratings.creeptimer'}">Creeps</b-button>
           <br />
           Creeps = Not so decent
         </div>
